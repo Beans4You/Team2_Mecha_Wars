eb953fbe118a98f8585285a49a4155a18f42f53f
howelrob <howellrobbie01@gmail.com>
2024-10-12 23:38:55 -0500

    working on stronghold

--- mecha-wars/scenes/enemies/enemy.gd
+++ mecha-wars/scenes/enemies/enemy.gd
@@ -3,18 +3,19 @@ class_name enemy
 
 @export var speed = 200
 var health = 10
+var damage = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite2D.play('move')
 	$AnimatedSprite2D.flip_h = true
-	
-	
+
 
 func _process(delta):
 	get_parent().set_progress(get_parent().get_progress() + speed * delta)
 	if get_parent().get_progress_ratio() == 1:
 		$AnimatedSprite2D.play('attack')
+	
 	
 	if health <=0:
 		get_parent().get_parent().queue_free()
--- mecha-wars/scenes/enemies/runner/runner.gd
+++ mecha-wars/scenes/enemies/runner/runner.gd
@@ -1,1 +1,8 @@
 extends enemy
+
+signal stronghold_attack(damage)
+
+func _on_animated_sprite_2d_animation_looped() -> void:
+	pass
+	#if $AnimatedSprite2D.animation == 'attack':
+		#stronghold_attack.emit(damage)
