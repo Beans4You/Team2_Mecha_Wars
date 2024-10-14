3bf82cfa04c878ecfb031e10ed0fa69652340b09
howelrob <howellrobbie01@gmail.com>
2024-10-14 01:29:41 -0500

    Enemy can now damage stronghold and destroy it

--- mecha-wars/scenes/enemies/enemy.gd
+++ mecha-wars/scenes/enemies/enemy.gd
@@ -3,7 +3,10 @@ class_name enemy
 
 @export var speed = 200
 var health = 10
-var damage = 5
+var attack_damage = 5
+
+var at_stronghold = false
+var stronghold
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,7 +17,15 @@ func _ready():
 func _process(delta):
 	get_parent().set_progress(get_parent().get_progress() + speed * delta)
 	if get_parent().get_progress_ratio() == 1:
-		$AnimatedSprite2D.play('attack')
+		queue_free()
+	
+	if at_stronghold:
+		if stronghold.visible:
+			speed = 0
+			$AnimatedSprite2D.play('attack')
+		else:
+			speed = 200
+			$AnimatedSprite2D.play('move')
 	
 	
 	if health <=0:
@@ -26,3 +37,15 @@ func on_hit(damage):
 	health -= damage
 	#if health <= 0:
 		#get_parent().get_parent().queue_free()
+
+
+#checking to see if enemy is at the stronghold
+func _on_hit_area_body_entered(body) -> void:
+	if body.is_in_group("stronghold"):
+		at_stronghold = true
+		stronghold = body
+
+#when attack animation loops, do damage to the stronghold
+func _on_animated_sprite_2d_animation_looped() -> void:
+	if $AnimatedSprite2D.animation == 'attack':
+		stronghold.take_damage(attack_damage)
