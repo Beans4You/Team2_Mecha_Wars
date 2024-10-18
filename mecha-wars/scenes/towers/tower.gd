65d75a87b045c5efbe06e857efdae8b7b6317f81
howelrob <howellrobbie01@gmail.com>
2024-10-18 11:50:48 -0500

    towers cost gold to place now

--- mecha-wars/scenes/towers/tower.gd
+++ mecha-wars/scenes/towers/tower.gd
@@ -3,13 +3,16 @@ extends CharacterBody2D
 class_name tower
 
 var tower_range = Vector2(350, 350)
-var gold_cost = 250
+var gold_cost = 300
 var paid_for = false
 
 
 func _onready():
 	$area.Size = tower_range
 	$area.position = -1 * tower_range / 2
+
+func _ready():
+	pass
 
 func pay_for():
 	if global_vars.curr_gold >= gold_cost and paid_for == false:
--- mecha-wars/scenes/ui/rocket_dino_panel.gd
+++ mecha-wars/scenes/ui/rocket_dino_panel.gd
@@ -6,36 +6,37 @@ var currTile
 
 func _on_gui_input(event):
 	var tempTower = tower_scene.instantiate()
-	var placement = placement_check.instantiate()
-
 	
-	if event is InputEventMouseButton and event.button_mask == 1:
-		#Left Click Down
-		add_child(tempTower)
-		tempTower.get_node("area").show()
+	if global_vars.curr_gold >= tempTower.gold_cost:
+		var placement = placement_check.instantiate()
 		
-		add_child(placement)
-		
+		if event is InputEventMouseButton and event.button_mask == 1:
+			#Left Click Down
+			add_child(tempTower)
+			tempTower.get_node("area").show()
+			
+			add_child(placement)
+			
+			tempTower.process_mode = Node.PROCESS_MODE_DISABLED
 
-		tempTower.process_mode = Node.PROCESS_MODE_DISABLED
-
-	elif event is InputEventMouseMotion and event.button_mask == 1:
-		#Left Click Drag
-		get_child(2).global_position = event.global_position
-		get_child(1).global_position = event.global_position
-	#This is the "drop" action for the dino
-	elif event is InputEventMouseButton and event.button_mask == 0:
-		#Left Click Up
-		#print("Left Button up")
-		if global_vars.can_place == true:
-			get_child(1).queue_free()
-			var path = get_tree().get_root()
-			path.add_child(tempTower)
-			tempTower.global_position = event.global_position
-			tempTower.get_node("area").hide()
-		else:
-			get_child(1).queue_free()
-			get_child(2).queue_free()
-	else:
-		if get_child_count() > 1:
+		elif event is InputEventMouseMotion and event.button_mask == 1:
+			#Left Click Drag
+			get_child(2).global_position = event.global_position
+			get_child(1).global_position = event.global_position
+		#This is the "drop" action for the dino
+		elif event is InputEventMouseButton and event.button_mask == 0:
+			#Left Click Up
+			#print("Left Button up")
+			if global_vars.can_place == true:
 				get_child(1).queue_free()
+				var path = get_tree().get_root()
+				path.add_child(tempTower)
+				tempTower.global_position = event.global_position
+				tempTower.get_node("area").hide()
+				global_vars.curr_gold -= tempTower.gold_cost
+			else:
+				get_child(1).queue_free()
+				get_child(2).queue_free()
+		else:
+			if get_child_count() > 1:
+					get_child(1).queue_free()
