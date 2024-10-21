50154af035eda5e7d2ef9097b61bf295a4d26683
Tu Ngo <ngotu1231@gmail.com>
2024-10-21 10:18:20 -0500

    Added tutorial to start screen

--- mecha-wars/scenes/ui/start_screen.gd
+++ mecha-wars/scenes/ui/start_screen.gd
@@ -16,3 +16,8 @@ func _on_button_pressed() -> void:
 	var timer = parent.get_node("Path_Spawner/Timer")
 	timer.start()
 	
+ 
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/gameplay and maps/tutorial.tscn") # Replace with function body.
