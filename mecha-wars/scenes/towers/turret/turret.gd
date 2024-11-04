1919b45525ada7e6720e31f5ceff57bcbf37f5b6
Blake Stanley <91094385+Blake-Stanley@users.noreply.github.com>
2024-11-04 01:47:23 -0600

    balance adjustments

--- mecha-wars/scenes/towers/turret/turret.gd
+++ mecha-wars/scenes/towers/turret/turret.gd
@@ -2,9 +2,8 @@ extends tower_projectile
 
 func _ready():
 	bullet_scene = preload("res://scenes/towers/turret/turret_laser.tscn")
-	rate_of_fire = 0.35
-extends tower_shooting
+	rate_of_fire = 0.37
+	
 
-func _ready():
 	super._ready()
 	self.gold_cost = 500
--- mecha-wars/turret_laser.gd
+++ mecha-wars/turret_laser.gd
@@ -3,4 +3,4 @@ extends bullet_parent
 func _ready():
 	explosion = null
 	speed = 800
-	bullet_damage = 1
+	bullet_damage = 2
