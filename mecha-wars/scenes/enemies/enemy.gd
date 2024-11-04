bbb78a7919bf1feda650cdc34ffc87d4fcf8a5b2
Blake Stanley <91094385+Blake-Stanley@users.noreply.github.com>
2024-11-04 14:30:18 -0600

    damage sound effects

--- mecha-wars/scenes/enemies/enemy.gd
+++ mecha-wars/scenes/enemies/enemy.gd
@@ -44,6 +44,7 @@ func _process(delta):
 # hit scan (tower shooting scene)
 func on_hit(damage):
 	health -= damage
+	$damaged_audio.play()
 	#if health <= 0:
 		#get_parent().get_parent().queue_free()
 
