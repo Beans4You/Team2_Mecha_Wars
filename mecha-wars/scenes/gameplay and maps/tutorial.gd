ce95679134e635fc5bb76cf4ce1049ea7ee57871
Tu Ngo <ngotu1231@gmail.com>
2024-11-04 20:38:02 -0600

    set up tutorial with max build

--- mecha-wars/scenes/gameplay and maps/tutorial.gd
+++ mecha-wars/scenes/gameplay and maps/tutorial.gd
@@ -2,7 +2,11 @@ extends Node
 
 @onready var can_place = false
 
-@onready var starting_gold = 500
+@onready var starting_gold = 800
 @onready var curr_gold = starting_gold
 
 @onready var game_over = false
+
+
+func _on_button_pressed() -> void:
+	$tutorial_path/Timer.start() # Replace with function body.
--- mecha-wars/scenes/gameplay and maps/tutorial_path.gd
+++ mecha-wars/scenes/gameplay and maps/tutorial_path.gd
@@ -6,12 +6,11 @@ extends Node2D
 @onready var main = preload("res://scenes/gameplay and maps/main.tscn")
 @onready var game_scene = get_parent()
 
-var enemy_array = [1, 1, 1]
+var enemy_array = [1, 1, 1, 1, 1, 1, 1, 1]
 var array_spot = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
-
 
 func _process(_delta):
 	if array_spot == len(enemy_array):
