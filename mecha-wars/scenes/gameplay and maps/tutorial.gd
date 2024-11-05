12bce2d8ea6a1cc1ab493019e769813d6e3174d0
Blake Stanley <91094385+Blake-Stanley@users.noreply.github.com>
2024-11-04 21:01:55 -0600

    tutorial update

--- mecha-wars/scenes/gameplay and maps/tutorial.gd
+++ mecha-wars/scenes/gameplay and maps/tutorial.gd
@@ -10,3 +10,17 @@ extends Node
 
 func _on_button_pressed() -> void:
 	$tutorial_path/Timer.start() # Replace with function body.
+
+func _physics_process(_delta: float) -> void:
+	if game_over:
+		tutorial_failed()
+	
+
+func tutorial_failed():
+		
+		var loss_screen_scene = load("res://scenes/ui/loss_screen.tscn")
+		var loss_screen = loss_screen_scene.instantiate()
+		loss_screen.level_path = "res://scenes/gameplay and maps/tutorial.tscn"
+		get_parent().add_child(loss_screen)
+		queue_free()
+		
