d0c6d433c7953ee5ae9dcf5f6cb15beae194b866
Blake Stanley <91094385+Blake-Stanley@users.noreply.github.com>
2024-11-11 18:25:20 -0600

    turret bug fix switching to projectile

--- mecha-wars/scenes/towers/turret/turret.gd
+++ mecha-wars/scenes/towers/turret/turret.gd
@@ -2,7 +2,7 @@ extends tower_projectile
 
 func _ready():
 	bullet_scene = preload("res://scenes/towers/turret/turret_laser.tscn")
-	rate_of_fire = 0.3
+	rate_of_fire = 0.33
 	tower_range = 600
 	gold_cost = 500
 
